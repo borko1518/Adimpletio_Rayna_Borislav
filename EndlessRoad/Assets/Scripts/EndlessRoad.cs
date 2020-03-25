66267efe9b108ecf78d412025995b3d850a4ec1d
borko1518 <62651579+borko1518@users.noreply.github.com>
2020-03-25 13:59:53 +0100

    EndlessRoad

---
+++ EndlessRoad/Assets/Scripts/EndlessRoad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessRoad : MonoBehaviour
+{
+    public Transform startPos;
+    int nextStep = 0;
+    public GameObject[] PrefabsRoad;
+    
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        nextStep += 10;
+        Instantiate(PrefabsRoad[Random.Range(0, PrefabsRoad.Length)], new Vector3(startPos.position.x, startPos.position.y, transform.position.z + nextStep), Quaternion.identity);
+    }
+}
